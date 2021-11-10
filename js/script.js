da45368fb548ed2cb596c934ade110757898d152
LarisaGimbu <gimbularisa@gmail.com>
2021-11-10 15:43:22 +0100

    color change

--- js/script.js
+++ js/script.js
@@ -8,6 +8,30 @@
 const app = new Vue({
   el:'#app',
   data:{
-    title:'Vue first site'
+    title:'Benvenuto',
+    userName:'',
+    headerColor:'',
+    textColor:'black',
+  },
+  methods:{
+    colorRed(){
+      this.headerColor = 'red';
+      this.changeTextColor();
+    },
+    colorYellow(){
+      this.headerColor = 'yellow';
+      this.changeTextColor();
+    },
+    colorBlue(){
+      this.headerColor = 'blue';
+      this.changeTextColor();
+    },
+    changeTextColor(){
+      if(this.headerColor==='red' || this.headerColor === 'blue'){
+        this.textColor = 'white';
+      }else{
+        this.textColor = 'black';
+      }
+    }
   }
 });
