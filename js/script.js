722e8521e2c639bfa4251989e4f7cf6e29a8a6c7
LarisaGimbu <gimbularisa@gmail.com>
2021-11-10 13:21:43 +0100

    title

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+/*
+  Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+  Bonus:
+  Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+
 const app = new Vue({
   el:'#app',
   data:{
-    
+    title:'Vue first site'
   }
 });
